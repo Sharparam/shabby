ab1e0f2f302ffc3d25becff38da76f05a961059e
Adam Hellberg <sharparam@sharparam.com>
2025-05-27 22:27:19 +0200

    Set up Nix flake and lorri
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I6a6a6964b47b82a2ec61cc4bcd1ee80cfe8d547b

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    naersk,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = (import nixpkgs) {inherit system overlays;};
+      toolchain = pkgs.rust-bin.stable.latest.default;
+      nativeBuildInputs = with pkgs; [
+        toolchain
+      ];
+      buildInputs = with pkgs; [];
+      naersk' = pkgs.callPackage naersk {
+        cargo = toolchain;
+        rustc = toolchain;
+        clippy = toolchain;
+      };
+    in let
+      app = naersk'.buildPackage {
+        src = ./.;
+        nativeBuildInputs = nativeBuildInputs;
+        buildInputs = buildInputs;
+      };
+    in {
+      defaultPackage = app;
+      packages = {
+        container = pkgs.dockerTools.buildImage {
+          name = "shabby";
+          config = {
+            entrypoint = ["${app}/bin/shabby"];
+          };
+        };
+      };
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [] ++ nativeBuildInputs;
+        buildInputs = with pkgs; [] ++ buildInputs;
+      };
+    });
+}
