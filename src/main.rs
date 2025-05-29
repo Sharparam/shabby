1a9f0a1a13e2e21db31a0f57cd5b4052eefb3d87
Adam Hellberg <sharparam@sharparam.com>
2025-05-29 02:07:42 +0200

    Add basic message listener
    Signed-off-by: Adam Hellberg <sharparam@sharparam.com>
    Change-Id: I6a6a6964ed8c02cbb9aaffcfe138585eb4e93b04

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,90 @@ use std::{env, io::Write};
 use color_eyre::{Result, eyre::WrapErr};
 
 use grammers_client::{
-    Client, Config as GrammersConfig,
-    grammers_tl_types::enums::{MessageAction, MessageReplyHeader},
+    Client, Config as GrammersConfig, InputMessage, Update,
+    grammers_tl_types::types::MessageMediaDice,
     session::Session,
+    types::{Media, User, media::Dice},
 };
 use shabby::logging;
-use tracing::info;
+use tokio::signal;
+
+#[allow(
+    unused_imports,
+    reason = "these will just constantly be added and removed otherwise"
+)]
+use tracing::{debug, error, info, warn};
 
 const SESSION_FILENAME: &str = "shabby.session";
+
+async fn handle_update(client: &Client, update: Update, me: &User) -> Result<bool> {
+    match update {
+        // Because we're making a userbot, we only care about messages sent by ourselves
+        Update::NewMessage(message) if message.sender().is_some_and(|s| s.id() == me.id()) => {
+            let chat = message.chat();
+            let chat_name = chat.name();
+
+            if let Some(Media::Dice(ref dice)) = message.media() {
+                if dice.raw.value != 6 {
+                    let reply_to = message.reply_to_message_id();
+
+                    message.delete().await?;
+
+                    let dice_media = Media::Dice(Dice {
+                        raw: MessageMediaDice {
+                            emoticon: "".to_string(),
+                            value: 0,
+                        },
+                    });
+
+                    let dice_msg = InputMessage::text("")
+                        .reply_to(reply_to)
+                        .copy_media(&dice_media)
+                        .silent(true);
+
+                    client.send_message(&chat, dice_msg).await?;
+                    return Ok(false);
+                }
+            }
+
+            info!(
+                "Message in {} ({}): {}",
+                chat_name,
+                chat.id(),
+                message.text()
+            );
+
+            let quit_command = message.text().trim().to_lowercase() == "!quit";
+
+            if quit_command {
+                info!("Received quit command");
+                if let Err(err) = message.delete().await {
+                    warn!("Failed to delete quit command message: {}", err);
+                }
+            }
+
+            Ok(quit_command)
+        }
+        // Update::Raw(raw) => {
+        //     debug!("Raw: {:?}", raw);
+        //     Ok(false)
+        // }
+        _ => Ok(false),
+    }
+}
+
+async fn handle_updates(client: Client, me: User) -> Result<()> {
+    loop {
+        let update = client.next_update().await?;
+        let quit = handle_update(&client, update, &me).await?;
+
+        if quit {
+            break;
+        }
+    }
+
+    Ok(())
+}
 
 /// The entry point for shabby.
 ///
@@ -86,39 +162,65 @@ async fn main() -> Result<()> {
         );
     }
 
+    let me = client.get_me().await.wrap_err("Failed to get self")?;
+
     info!("Successfully connected and authorized");
 
-    let topic_channel_id = env::var("SHABBY_TG_TOPIC_CHANNEL")
-        .wrap_err("Topic channel ID not set")?
-        .parse::<i64>()
-        .wrap_err("Topic channel ID must be a number")?;
+    println!("Press Ctrl+C to exit");
 
-    let mut dialogs = client.iter_dialogs();
-
-    while let Some(dialog) = dialogs.next().await? {
-        let chat = dialog.chat();
-        let id = chat.id();
-        if id == topic_channel_id {
-            println!("{} ({})", chat.name(), chat.id());
-            let mut messages = client.iter_messages(chat);
-
-            while let Some(message) = messages.next().await? {
-                if let Some(MessageAction::TopicCreate(topic)) = message.action() {
-                    println!("[{}] <topic> {}", message.id(), topic.title);
-                }
-                if message.pinned() {
-                    if let Some(MessageReplyHeader::Header(header)) = &message.raw.reply_to {
-                        if header.forum_topic {
-                            println!("[{}] <topic pin> {}", message.id(), message.text());
-                        }
-                    }
-                }
+    tokio::select! {
+        _ = signal::ctrl_c() => {
+            info!("Received SIGINT, exiting");
+        }
+        update_result = handle_updates(client.clone(), me) => {
+            match update_result {
+                Ok(_) => info!("Disconnected from Telegram gracefully"),
+                Err(e) => error!("Error while handling updates: {}", e),
             }
         }
     }
 
+    info!("Saving session file and exiting");
+    client
+        .session()
+        .save_to_file(SESSION_FILENAME)
+        .wrap_err("Failed to save session on exit")?;
+
     Ok(())
 }
+
+// async fn debug_topics(client: Client) -> Result<()> {
+//     let topic_channel_id = env::var("SHABBY_TG_TOPIC_CHANNEL")
+//         .wrap_err("Topic channel ID not set")?
+//         .parse::<i64>()
+//         .wrap_err("Topic channel ID must be a number")?;
+
+//     let mut dialogs = client.iter_dialogs();
+
+//     while let Some(dialog) = dialogs.next().await? {
+//         let chat = dialog.chat();
+//         let id = chat.id();
+//         if id == topic_channel_id {
+//             println!("{} ({})", chat.name(), chat.id());
+//             let mut messages = client.iter_messages(chat);
+
+//             while let Some(message) = messages.next().await? {
+//                 if let Some(MessageAction::TopicCreate(topic)) = message.action() {
+//                     println!("[{}] <topic> {}", message.id(), topic.title);
+//                 }
+//                 if message.pinned() {
+//                     if let Some(MessageReplyHeader::Header(header)) = &message.raw.reply_to {
+//                         if header.forum_topic {
+//                             println!("[{}] <topic pin> {}", message.id(), message.text());
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     Ok(())
+// }
 
 fn get_from_env_or_stdin(var_name: &str) -> Result<String> {
     env::var(var_name).or_else(|_| {
